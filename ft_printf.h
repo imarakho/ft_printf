b6292f65afbed12fcaaa98bbedccaab593052d90
Igor Marakhov <imarakho@e1r7p6.unit.ua>
2018-02-18 15:54:19 +0200

    ft

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: imarakho <imarakho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 13:23:09 by imarakho          #+#    #+#             */
-/*   Updated: 2018/02/18 15:38:33 by imarakho         ###   ########.fr       */
+/*   Updated: 2018/02/18 15:54:09 by imarakho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ typedef struct      s_par
     int             res;
     unsigned int    pres;
     char            sz;
-    size_t          val;
+    intmax_t        val;
 }                   t_par;
 
 char	*ft_itoa_base(long value, int base);
