fb0d1f891d45ad60598086711ba8f1d53d04b363
Igor Marakhov <imarakho@e1r7p6.unit.ua>
2018-02-18 17:29:37 +0200

    ft

--- check_int.c
+++ check_int.c
@@ -6,7 +6,7 @@
 /*   By: imarakho <imarakho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 15:28:28 by imarakho          #+#    #+#             */
-/*   Updated: 2018/02/18 17:22:29 by imarakho         ###   ########.fr       */
+/*   Updated: 2018/02/18 17:29:27 by imarakho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void    check_int(t_par *pr, va_list *ap)
 {
 	//pr->val = va_arg(*ap, int);
-	//pr->val = va_arg(*ap, intmax_t);
+	pr->val = va_arg(*ap, intmax_t);
 	make_size(pr, 'd', ap);
 	pr->s = ft_itoa_base(pr->val, 10);
 	if (!pr->minus)
@@ -41,7 +41,7 @@ void    check_uns_int(t_par *pr, va_list *ap)
 {
   //  *i += 1;
 	//pr->uval = va_arg(*ap, unsigned int);
-	//pr->uval = va_arg(*ap, uintmax_t);
+	pr->uval = va_arg(*ap, uintmax_t);
 	make_size(pr, 'u', ap);
 	pr->s = ft_unsitoa_base(pr->uval, 10);
 	//pr->s = ft_itoa_base(va_arg(*ap, unsigned int), 10);
@@ -64,7 +64,7 @@ void    check_uns_int(t_par *pr, va_list *ap)
 void    check_octal(t_par *pr, va_list *ap)
 {
 	//pr->uval = va_arg(*ap, unsigned int);
-	//pr->uval = va_arg(*ap, uintmax_t);
+	pr->uval = va_arg(*ap, uintmax_t);
 	make_size(pr, 'o', ap);
 	pr->s = ft_unsitoa_base(pr->uval, 8);
 	//pr->s = ft_itoa_base(va_arg(*ap, unsigned int), 8);
@@ -93,7 +93,7 @@ void    check_octal(t_par *pr, va_list *ap)
 void    check_hex(t_par *pr, va_list *ap)
 {
 	//pr->uval = va_arg(*ap, unsigned int);
-	//pr->uval = va_arg(*ap, uintmax_t);
+	pr->uval = va_arg(*ap, uintmax_t);
 	make_size(pr, 'x', ap);
 	pr->s = ft_unsitoa_base(pr->uval, 16);
 	//pr->s = ft_itoa_base(va_arg(*ap, unsigned int), 16);
@@ -130,7 +130,7 @@ void    check_hex(t_par *pr, va_list *ap)
 void    check_heX(t_par *pr, va_list *ap)
 {
 	//pr->val = va_arg(*ap, intmax_t);
-	//pr->uval = va_arg(*ap, uintmax_t);
+	pr->uval = va_arg(*ap, uintmax_t);
 	make_size(pr, 'X', ap);
 	pr->s = ft_unsitoa_base(pr->uval, 16);
 	//pr->s = ft_itoa_base(va_arg(*ap, unsigned int), 16);
