64b454a0f8380a1e07e8cbb0fc393a3bb389ff5f
Igor Marakhov <imarakho@e1r7p14.unit.ua>
2018-02-26 14:42:21 +0200

    ft

--- check_strings.c
+++ check_strings.c
@@ -6,7 +6,7 @@
 /*   By: imarakho <imarakho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 14:21:58 by imarakho          #+#    #+#             */
-/*   Updated: 2018/02/26 14:40:19 by imarakho         ###   ########.fr       */
+/*   Updated: 2018/02/26 14:41:47 by imarakho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,10 @@ void    check_string(t_par *pr, va_list *ap)
 		else
 			ft_putstr(pr->s);
 			if(pr->pres > 1 && pr->pres < ft_strlen(pr->s) && pr->space > pr->pres)
+			{
 				pr->space = pr->space - pr->pres;
+				pr->space++;
+			}
 			make_width(pr , 's');
 	}
 }
